f8de2077138d14cf6b8cbc94eaf8d75d902f80ae
Geunyoung Kim <kgysmart@gmail.com>
2018-10-19 10:04:22 -0700

    Add SpecualPriceRule

--- OCP/Model/SpecialPriceRule.cs
+++ OCP/Model/SpecialPriceRule.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 namespace OCP.Model
 {
-    public class SpecialPriceRule
+    public class SpecialPriceRule : IPriceRule
     {
-        public SpecialPriceRule()
+        public decimal CalculatePrice(OrderItem item)
         {
+            decimal total = 0m;
+            total += item.Quantity * .4m;
+            int setsOfThree = item.Quantity / 3;
+            total -= setsOfThree * .2m;
+            return total;
+        }
+
+        public bool IsMatch(OrderItem item)
+        {
+            return item.Sku.StartsWith("SPECIAL", StringComparison.CurrentCultureIgnoreCase);
         }
     }
 }

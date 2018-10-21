2537b4543e2c033faa10c2dfd61274f6bed77995
Geunyoung Kim <kgysmart@gmail.com>
2018-10-20 21:59:13 -0700

    Add CartShould test case

---
+++ OCPTest/Model/CartShould.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OCPTest.Model
+{
+    public class CartShould
+    {
+        private readonly ITestOutputHelper _output;
+        private readonly OCP.Model.Cart _sut;
+
+        public CartShould(ITestOutputHelper output)
+        {
+            this._output = output;
+            this._sut = new OCP.Model.Cart();
+        }
+
+        [Fact]
+        public void ZeroWhenEmpty()
+        {
+            Assert.Equal(0, _sut.TotalAmount());
+        }
+    }
+}

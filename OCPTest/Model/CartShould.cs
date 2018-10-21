8e82aadc8431581cd02e70103bb4e8385aa29a7b
Geunyoung Kim <kgysmart@gmail.com>
2018-10-20 22:04:57 -0700

    Add FiveWithOneEachItem testcase

--- OCPTest/Model/CartShould.cs
+++ OCPTest/Model/CartShould.cs
@@ -1,24 +1,32 @@
 ﻿using System;
 using Xunit;
 using Xunit.Abstractions;
+using OCP.Model;
 
 namespace OCPTest.Model
 {
     public class CartShould
     {
         private readonly ITestOutputHelper _output;
-        private readonly OCP.Model.Cart _sut;
+        private readonly Cart _sut;
 
         public CartShould(ITestOutputHelper output)
         {
             this._output = output;
-            this._sut = new OCP.Model.Cart();
+            this._sut = new Cart();
         }
 
         [Fact]
         public void ZeroWhenEmpty()
         {
             Assert.Equal(0, _sut.TotalAmount());
+        }
+
+        [Fact]
+        public void FiveWithOneEachItem()
+        {
+            _sut.Add(new OrderItem() { Quantity = 1, Sku = "EACH_WIDGET" });
+            Assert.Equal(5.0m, _sut.TotalAmount());
         }
     }
 }

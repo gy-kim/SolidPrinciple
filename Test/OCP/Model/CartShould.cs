a66562df7ad9f7e9a5a7e2a6eaa407cb40c79cd3
Geunyoung Kim <kgysmart@gmail.com>
2018-10-21 22:56:15 -0700

    Modify test project

---
+++ Test/OCP/Model/CartShould.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+using OCP.Model;
+
+namespace OCPTest.Model
+{
+    [Trait("Category", "OCP_Cart")]
+    public class CartShould
+    {
+        private readonly ITestOutputHelper _output;
+        private readonly Cart _sut;
+
+        public CartShould(ITestOutputHelper output)
+        {
+            this._output = output;
+            this._sut = new Cart();
+        }
+
+        [Fact]
+        public void ZeroWhenEmpty()
+        {
+            Assert.Equal(0, _sut.TotalAmount());
+        }
+
+        [Fact]
+        public void FiveWithOneEachItem()
+        {
+            _sut.Add(new OrderItem() { Quantity = 1, Sku = "EACH_WIDGET" });
+            Assert.Equal(5.0m, _sut.TotalAmount());
+        }
+
+        [Fact]
+        public void TwoWithHalfKiloWeightItem()
+        {
+            this._sut.Add(new OrderItem { Quantity = 500, Sku = "WEIGHT_PEANUTS" });
+            Assert.Equal(2m, _sut.TotalAmount());
+        }
+
+        [Fact]
+        public void EightyCentsWithTwoSpecialItem()
+        {
+            this._sut.Add(new OrderItem { Quantity = 2, Sku = "SPECIAL_CANDYBAR" });
+            Assert.Equal(0.80m, _sut.TotalAmount());
+        }
+
+        [Fact]
+        public void TwoDollarsWithSixSpecialItem()
+        {
+            this._sut.Add(new OrderItem { Quantity = 6, Sku = "SPECIAL_CANDYBAR" });
+            Assert.Equal(2m, _sut.TotalAmount());
+        }
+
+        [Fact]
+        public void FourDollarsWithFourBuy4Get1FreeItems()
+        {
+            this._sut.Add(new OrderItem { Quantity = 4, Sku = "B4GO_APPLE" });
+            Assert.Equal(4m, _sut.TotalAmount());
+        }
+
+        [Fact]
+        public void FoutDollarsWithFiveBuy4Get1FreeItems()
+        {
+            this._sut.Add(new OrderItem { Quantity = 5, Sku = "B4GO_APPLE" });
+            Assert.Equal(4m, _sut.TotalAmount());
+        }
+    }
+}

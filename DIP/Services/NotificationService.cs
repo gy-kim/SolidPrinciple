8a61230b1626d6accc167734a61c6cfc4f57fcf9
Geunyoung Kim <kgysmart@gmail.com>
2018-10-22 13:15:56 -0700

    Implement NotificationService

--- DIP/Services/NotificationService.cs
+++ DIP/Services/NotificationService.cs
@@ -1,10 +1,35 @@
 ﻿using System;
+using System.Net.Mail;
+using DIP.Interfaces;
+using DIP.Model;
+
 namespace DIP.Services
 {
-    public class NotificationService
+    public class NotificationService : INotificationService
     {
-        public NotificationService()
+        public void NotifyCustomerOrderCreated(Cart cart)
         {
+            var customerEmail = cart.CustomerEmail;
+            if (!string.IsNullOrEmpty(customerEmail))
+            {
+                using (var message = new MailMessage("orders@somewhere.com", customerEmail))
+                {
+                    using (var client = new SmtpClient("localhost"))
+                    {
+                        message.Subject = $"Your order placed on {DateTime.Now}";
+                        message.Body = $"Your order detailed: \n {cart}";
+
+                        try
+                        {
+                            client.Send(message);
+                        }
+                        catch(Exception ex)
+                        {
+                            throw ex;
+                        }
+                    }
+                }
+            }
         }
     }
 }

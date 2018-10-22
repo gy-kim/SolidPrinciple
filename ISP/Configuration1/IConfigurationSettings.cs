95bb3d0e29ddc80a011b0cd17a8c4ef1d46e7cfa
Geunyoung Kim <kgysmart@gmail.com>
2018-10-22 09:16:54 -0700

    Add ISP IConfigurationSettings

---
+++ ISP/Configuration1/IConfigurationSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ISP.Configuration1
+{
+    public interface IConfigurationSettings
+    {
+        string ApplicationName { get; }
+        string AuthorName { get; }
+
+        int CachDuration { get; }
+
+        string DatabaseServerName { get; }
+        string DatabaseName { get; }
+        string DatabaseUserName { get; }
+        string DatabasePassword { get; }
+
+        string WebServiceBaseUri { get; }
+    }
+}

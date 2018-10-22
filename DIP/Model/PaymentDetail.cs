d63f5d2962fed0be7f5a7a55f28fd9fa668bb2a6
Geunyoung Kim <kgysmart@gmail.com>
2018-10-22 10:53:34 -0700

    Implement PaymentDetail

--- DIP/Model/PaymentDetail.cs
+++ DIP/Model/PaymentDetail.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 namespace DIP.Model
 {
+    public enum PaymentMethod
+    {
+        Cash,
+        CreditCard
+    }
+
     public class PaymentDetail
     {
-        public PaymentDetail()
-        {
-        }
+        public PaymentMethod PaymentMethod { get; set; }
+
+        public string CreditCardNumber { get; set; }
+
+        public string ExpiresMonth { get; set; }
+
+        public string ExpiresYear { get; set; }
+
+        public string CardHolderName { get; set; }
     }
 }
